241a0e73dd17363959c949ecae906d58f60f6739
Jose Sandoval <sm18003@ues.edu.sv>
2021-08-12 19:23:47 -0600

    Modificado Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,7 +30,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Enlaces
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
